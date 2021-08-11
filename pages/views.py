90d7af7ec1b75df4e738052b380da2b576f8905f
JoshuQP <jpalmierdevelopment@gmail.com>
2021-08-10 20:40:54 -0600

    firstcommit

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+
+class AboutPageView(TemplateView):
+    template_name ="about.html"
+
+# Create your views here.
+
+
